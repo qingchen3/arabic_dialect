1c9e1b17757662b497d1cc94635b55bf9695879e
qingchen3 <qchen.db@gmail.com>
2017-05-10 13:35:21 +0300

    changing the route and view mapping for audio uploading

--- scaffold/__init__.py
+++ scaffold/__init__.py
@@ -1,6 +1,5 @@
 from pyramid.config import Configurator
 from wsgiref.simple_server import make_server
-from waitress import serve
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -9,19 +8,18 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
-    config.add_route('home', '/')
-    config.add_route('client', '/client')
-    #config.add_route('test', '/test')
-    config.add_route('store_mp3_form', '/store_mp3_form')
-    config.add_route('store_mp3_view', '/store_mp3_view')
 
-    config.add_route('store_mp3_test', '/store_mp3_test')
-    #config.add_route('store_audio_form', '/store_audio_form')
+    config.add_route('index', '/')
+
+    config.add_route('store_text_view', '/store_text_view')
+    config.add_route('submit_audio_view', '/submit_audio_view')
 
     config.scan()
+
     app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 8080, app)
-    #serve(app, listen='0.0.0.0:8080', url_scheme='https')
+    port = 8080
+    server = make_server('0.0.0.0', port, app)
     server.serve_forever()
 
-    return app
+
+    return config.make_wsgi_app()
